34988efb7e11f83c9a14691c480a6eca7d6ca074
Haris Samingan <muhamad_haris@hotmail.com>
2023-07-04 18:41:21 +0800

    Refactor to side bar

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,8 +1,8 @@
 import { useEffect } from "react";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import useLocalStorage from "use-local-storage";
-import IconButton from "../components/IconButton";
+import ProfileSideBar from "../components/ProfileSideBar";
 
 export default function ProfilePage() {
   const [authToken, setAuthToken] = useLocalStorage("authToken", "");
@@ -16,26 +16,7 @@ export default function ProfilePage() {
     <>
       <Container>
         <Row>
-          <Col
-            sm={2}
-            className="d-flex flex-column justify-content-start align-items-start bg-light vh-100"
-            style={{ position: "sticky", top: 0 }}
-          >
-            <IconButton className="bi bi-twitter" isTop />
-            <IconButton className="bi bi-house" text="Home" />
-            <IconButton className="bi bi-search" text="Explore" />
-            <IconButton className="bi bi-bell" text="Notifications" />
-            <IconButton className="bi bi-envelope" text="Messages" />
-            <IconButton className="bi bi-journal-text" text="Lists" />
-            <IconButton className="bi bi-bookmark" text="Bookmarks" />
-            <IconButton className="bi bi-patch-check" text="Verified" />
-            <IconButton className="bi bi-person" text="Profile" />
-            <IconButton className="bi bi-filter-circle" text="More" />
-            <IconButton className="bi bi-door-closed" text="Logout" />
-            <Button onClick={handleLogout} className="rounded-pill w-100 mb-3">
-              Tweet
-            </Button>
-          </Col>
+          <ProfileSideBar handleLogout={handleLogout} />
         </Row>
       </Container>
     </>
