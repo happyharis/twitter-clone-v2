209c0e8334ceb2cb7a2725e1632fd3db0ba81eb8
Haris Samingan <muhamad_haris@hotmail.com>
2023-07-04 18:37:41 +0800

    Add sidebar

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
-import { Button, Container, Navbar } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import useLocalStorage from "use-local-storage";
+import IconButton from "../components/IconButton";
 
 export default function ProfilePage() {
   const [authToken, setAuthToken] = useLocalStorage("authToken", "");
@@ -13,21 +14,29 @@ export default function ProfilePage() {
   const handleLogout = () => setAuthToken("");
   return (
     <>
-      <Navbar bg="light">
-        <Container>
-          <Navbar.Brand href="/">
-            <i
-              className="bi bi-twitter"
-              style={{ fontSize: 30, color: "dodgerblue" }}
-            ></i>
-          </Navbar.Brand>
-          <Navbar.Collapse className="justify-content-end">
-            <Button onClick={handleLogout}>Logout</Button>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <Container className="mt-3">
-        <h2> Your profile </h2>
+      <Container>
+        <Row>
+          <Col
+            sm={2}
+            className="d-flex flex-column justify-content-start align-items-start bg-light vh-100"
+            style={{ position: "sticky", top: 0 }}
+          >
+            <IconButton className="bi bi-twitter" isTop />
+            <IconButton className="bi bi-house" text="Home" />
+            <IconButton className="bi bi-search" text="Explore" />
+            <IconButton className="bi bi-bell" text="Notifications" />
+            <IconButton className="bi bi-envelope" text="Messages" />
+            <IconButton className="bi bi-journal-text" text="Lists" />
+            <IconButton className="bi bi-bookmark" text="Bookmarks" />
+            <IconButton className="bi bi-patch-check" text="Verified" />
+            <IconButton className="bi bi-person" text="Profile" />
+            <IconButton className="bi bi-filter-circle" text="More" />
+            <IconButton className="bi bi-door-closed" text="Logout" />
+            <Button onClick={handleLogout} className="rounded-pill w-100 mb-3">
+              Tweet
+            </Button>
+          </Col>
+        </Row>
       </Container>
     </>
   );
