8b670b2f0cde73865a77778118e09f4727132099
Haris Samingan <muhamad_haris@hotmail.com>
2023-07-06 12:19:53 +0800

    Add add post

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,4 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import jwtDecode from "jwt-decode";
 
 const BASE_URL =
   "https://twitter-api-sigmaschooltech.sigma-school-full-stack.repl.co";
@@ -12,6 +14,24 @@ export const fetchPostsByUser = createAsyncThunk(
   }
 );
 
+export const savePost = createAsyncThunk(
+  "posts/savePost",
+  async (postContent) => {
+    const token = localStorage.getItem("authToken");
+    const decode = jwtDecode(token);
+    const userId = decode.id;
+
+    const data = {
+      title: "Post Title",
+      content: postContent,
+      user_id: userId,
+    };
+
+    const response = await axios.post(`${BASE_URL}/posts`, data);
+    return response.data;
+  }
+);
+
 // Slice
 const postsSlice = createSlice({
   name: "posts",
@@ -21,7 +41,10 @@ const postsSlice = createSlice({
     builder.addCase(fetchPostsByUser.fulfilled, (state, action) => {
       state.posts = action.payload;
       state.loading = false;
-    });
+    }),
+      builder.addCase(savePost.fulfilled, (state, action) => {
+        state.posts = [action.payload, ...state.posts];
+      });
   },
 });
 
