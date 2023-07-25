af5f731ec6e3a3dc2410705f8f72d7116bb90f85
Haris Samingan <muhamad_haris@hotmail.com>
2023-07-25 20:35:38 +0800

    Add profile page

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,26 +1,3 @@
-import { useEffect } from "react";
-import { Container, Row } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
-import useLocalStorage from "use-local-storage";
-import ProfileMidBody from "../components/ProfileMidBody";
-import ProfileSideBar from "../components/ProfileSideBar";
-
 export default function ProfilePage() {
-  const [authToken, setAuthToken] = useLocalStorage("authToken", "");
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!authToken) navigate("/login");
-  }, [authToken, navigate]);
-  const handleLogout = () => setAuthToken("");
-  return (
-    <>
-      <Container>
-        <Row>
-          <ProfileSideBar handleLogout={handleLogout} />
-          <ProfileMidBody />
-        </Row>
-      </Container>
-    </>
-  );
+  return <div>ProfilePage</div>;
 }
